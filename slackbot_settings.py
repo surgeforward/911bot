3ef065aec9e143583af8a73bf081ce48387d8441
Sohail Somani <Sohail Somani>
2016-11-12 13:16:20 -0500

    Move API_TOKEN stuff to slackbot_settings.py, and trim the value
    This will probably fix the issue that Justin saw with it not accepting
    the environment variable

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,3 +1,5 @@
+import os
+
 DEFAULT_REPLY = "Sorry but I didn't understand you. Try `help`"
 DEBUG=1
 
@@ -5,3 +7,7 @@ PLUGINS = [
     'slackbot.plugins',
     'bot.plugins'
 ]
+
+if not os.environ.has_key("SLACKBOT_API_TOKEN"):
+   raise RuntimeError,"Missing env variable: SLACKBOT_API_TOKEN"
+API_TOKEN=os.environ['SLACKBOT_API_TOKEN'].strip()
